0f7a8bf8d71f69bbb18f81e05ad3ac5c3763fc96
Damien Dumontet <damien.dumontet@protonmail.com>
2024-02-08 00:07:02 -0300

    add tailwindcss

--- templates/home_templ.go
+++ templates/home_templ.go
@@ -29,7 +29,7 @@ func Home() templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h1>Hello, World!</h1></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div><h1 class=\"bg-teal-500\">Hello, World! From go / htmx template</h1></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
